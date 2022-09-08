e503bb1f9923118f1e55977bcbccc1f424b69fbe
Gab <gabmainacc1@gmail.com>
2022-09-08 12:45:10 +0800

    gabpoge

---
+++ gab.py
@@ -0,0 +1,9 @@
+import streamlit
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
